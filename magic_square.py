7ade30fc0d31c2bf0b000748a4583cfb85c75a38
Md. Khairul Anam <anam.simon@gmail.com>
2022-12-05 23:48:24 -0700

    Update magic_square.py
    add comment

--- magic_square.py
+++ magic_square.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+#returns magic square N x N
 def generate_magic_square(N):
 
     magic_square = np.zeros((N,N), dtype=int)
@@ -18,6 +19,7 @@ def generate_magic_square(N):
 
     return magic_square
 
+#randomly generate missing numbers in the magix square
 def generate_missing_number(magic_square, N, missing_number):
 
     missing_square = [[''] * N for i in range(N)]
@@ -32,12 +34,14 @@ def generate_missing_number(magic_square, N, missing_number):
             missing_square[i][j] = "X"
             n += 1
     return missing_square
-
+    
+#print magic square
 def print_matrix(square):
     for i in square:
         print('\t'.join(map(str, i)))
     print('------------')
-
+    
+#calculate magix sum
 def MagicSquareSum(array) :
     n = len(array)
     sum1=0
@@ -45,49 +49,46 @@ def MagicSquareSum(array) :
         sum1+=array[i][i]
     return sum1  
 
-cont = "y"
-
-while(cont=='y'):
-    try:
-        N = int(input("Magic Number N X N ? "))
-        
-        missing_numbers = int(input("Missing Numbers ? "))
-        
-        magic_square = generate_magic_square(N)
-        missing_square = generate_missing_number(magic_square,N,missing_numbers)
-        
-        print_matrix(magic_square)
-        
-        count = 0
-        limit = missing_numbers + 2
-        while(count < limit and missing_numbers > 0):
-                count=count + 1
-                print_matrix(missing_square)
-                print("Guess missing number X")
-                row = int(input("Row (1...N): "))
-                col = int(input("Column (1...N): "))
-                guess = int(input("Missing number: "))
-                
-                if(magic_square[row-1][col-1]==guess):
-                    print("Correct")
-                    missing_square[row-1][col-1]=guess
-                    missing_numbers=missing_numbers - 1
-                else:
-                    print("Wrong guess")
-                print("Attempt remaining ", limit - count)
-                print('------------')
+#main program flow
+def main():
+    cont = "y"
+    while(cont=='y'):
+        try:
+            N = int(input("Magic Number N X N ? "))
+            
+            missing_numbers = int(input("Missing Numbers ? "))
+            
+            magic_square = generate_magic_square(N)
+            missing_square = generate_missing_number(magic_square,N,missing_numbers)
+            
+            count = 0
+            limit = missing_numbers + 2
+            while(count < limit and missing_numbers > 0):
+                    count=count + 1
+                    print_matrix(missing_square)
+                    print("Guess missing number X")
+                    row = int(input("Row (1...N): "))
+                    col = int(input("Column (1...N): "))
+                    guess = int(input("Missing number: "))
                     
-        if(missing_numbers==0):
-            print("You have won!")
-        else:
-            print("Game Over! Sorry you lost.")
-        print("The Magic sum is ", MagicSquareSum(magic_square))
+                    if(magic_square[row-1][col-1]==guess):
+                        print("Correct")
+                        missing_square[row-1][col-1]=guess
+                        missing_numbers=missing_numbers - 1
+                    else:
+                        print("Wrong guess")
+                    print("Attempt remaining ", limit - count)
+                    print('------------')
+                        
+            if(missing_numbers==0):
+                print("You have won!")
+            else:
+                print("Game Over! Sorry you lost.")
+            print("The Magic sum is ", MagicSquareSum(magic_square))
+            
+            cont = input("Play again? [y/n] ")
         
-        cont = input("Play again? [y/n] ")
-    
-    except:
-        print("Something went wrong. Play again.")
-
-print("Thank you for playing the game. Bye")         
-      
-     
+        except:
+            print("Something went wrong. Play again.")
+main()
+print("Thank you for playing the game. Bye")
